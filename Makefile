dbe66687835e349751d03d85e6f3def526fbc1e9
mr. blue <gautam.chekuri@azri.biz>
2009-08-20 22:57:08 +0530

    1) added jni header file for native method 2) Implemented native method which calls wow_install_root to return java string in dll

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
+zugslist_jni_dll:
+	c:\MinGW\bin\gcc.exe -o zugslist_jni.dll -shared -Wall -Wl,-soname -I"c:\Program Files\Java\jdk1.5.0_20\include" -I"c:\Program Files\Java\jdk1.5.0_20\include\win32" zugslist_jni_dll.c -static
 zugslist_dll:
 	c:\MinGW\bin\gcc.exe -shared zugslist_dll.c -o zugslist_dll.dll
 zugslist_user:
 	c:\MinGW\bin\gcc.exe use_zugslist_dll.c zugslist_dll.dll -o use_zugslist_dll
 clean:
 	if exist zugslist_dll.dll del zugslist_dll.dll
+	if exist zugslist_jni.dll del zugslist_jni.dll
 	if exist use_zugslist_dll.exe del use_zugslist_dll.exe
 all: clean zugslist_dll zugslist_user
+jni: clean zugslist_jni_dll
---
+++ zugslist_jni_dll.c
@@ -0,0 +1,50 @@
+#include <windows.h>
+#include <winreg.h>
+#include <stdlib.h>
+#include <jni.h>
+
+#include "Uploader.h"
+#include "zugslist_dll.h"
+
+int wow_install_root(char *buff, DWORD *buff_size) {
+	HKEY wow_key;
+	DWORD var_type = REG_SZ;
+	LONG status;
+
+	RegOpenKeyEx(
+		HKEY_LOCAL_MACHINE,
+		"Software\\Blizzard Entertainment\\World of Warcraft",
+		0,
+		KEY_READ,
+		&wow_key
+	);
+
+	/* 
+	   LONG WINAPI RegQueryValueEx(
+		__in         HKEY hKey,
+		__in_opt     LPCTSTR lpValueName,
+		__reserved   LPDWORD lpReserved,
+		__out_opt    LPDWORD lpType,
+		__out_opt    LPBYTE lpData,
+		__inout_opt  LPDWORD lpcbData
+	);
+	*/
+	status = RegQueryValueEx(wow_key, "InstallPath", NULL, &var_type, buff, buff_size);
+	RegCloseKey (wow_key);
+	
+	return status;
+}
+
+JNIEXPORT jstring JNICALL Java_Uploader_getWoWPath(JNIEnv *jenv, jobject jobj) {
+	DWORD buff_size = 6;
+	char *buff = (char *)malloc(buff_size);
+	LONG status = wow_install_root(buff, &buff_size);
+  
+	if( status == ERROR_MORE_DATA ) {
+		buff = (char *)realloc((void *)buff, buff_size);
+		status = wow_install_root(buff, &buff_size);
+	}
+	jstring wow_path = (*jenv)->NewStringUTF(jenv,buff);
+	free((void *)buff);
+	return wow_path;
+}
